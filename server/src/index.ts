3ea97b4863e93b8a9a4d286505526a8697260e0f
Noel Gomez <nollgmz4@gmail.com>
2020-07-02 19:44:15 -0700

    app.ts -> index.ts name change

---
+++ server/src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import helmet from "helmet";
+import cors from "cors"
+import { router as quarterRouter } from "./routes/quarter-info";
+import { router as coursesRouter } from "./routes/courses-info";
+import { errorMiddleware } from "./Middleware/http-exception";
+
+const port = process.env.PORT || 4000;
+
+const app = express();
+
+app.use(helmet())
+app.use(cors());
+
+app.use("/quarters", quarterRouter);
+app.use("/courses", coursesRouter);
+
+app.use(errorMiddleware)
+
+
+
+
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`);
+
+})
