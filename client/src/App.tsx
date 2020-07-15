55510e4c00cb865dd2f83cc87c0241fa0ef5d24e
Noel Gomez <nollgmz4@gmail.com>
2020-07-14 20:44:03 -0700

    added quarter select to app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
 import { CourseSelector, SlugHeader } from './components';
+import QuarterSelect from './components/QuarterSelect/QuarterSelect';
 function App() {
   return (
     <div className="App">
       <SlugHeader />
+      <QuarterSelect />
     </div>
   );
 }
