37780aecbbfa3915fb4aa4c9401f968a561fb571
Noel Gomez <nollgmz4@gmail.com>
2020-07-14 20:44:32 -0700

    created quarterselect and edited dropdown menu

--- client/src/components/DropDownMenu/DropDownMenu.tsx
+++ client/src/components/DropDownMenu/DropDownMenu.tsx
@@ -1,4 +1,5 @@
 import React, { useState, ReactElement } from 'react';
+import "./DropDownMenu.scss";
 
 export interface Option<T> {
     value: T,
@@ -7,7 +8,7 @@ export interface Option<T> {
 
 
 interface MenuProps<T> {
-    defaultVal: Option<T>,
+    initialValue: T,
     options: Option<T>[]
     onClickOption: (arg: T) => void
 }
@@ -21,11 +22,11 @@ interface OptionProps<T> {
 function DropDownOption<T>({ option: { value, label }, optionAction }: OptionProps<T>): ReactElement<OptionProps<T>> {
     const onClick = () => { optionAction(value) }
     return (
-        <li onClick={onClick}>{label}</li>
+        <li className="option" onClick={onClick}>{label}</li>
     );
 }
 
-function DropDownMenu<T>({ defaultVal, options, onClickOption }: MenuProps<T>): ReactElement<MenuProps<T>> {
+function DropDownMenu<T>({ initialValue, options, onClickOption }: MenuProps<T>): ReactElement<MenuProps<T>> {
     let [dropDownActive, setDropDownActive] = useState(false);
 
     const toggleDropDown = () => setDropDownActive(!dropDownActive);
@@ -33,12 +34,16 @@ function DropDownMenu<T>({ defaultVal, options, onClickOption }: MenuProps<T>): 
         toggleDropDown();
         onClickOption(value)
     }
+    let initialOption = options.find(op => op.value === initialValue);
+    let leftOverOptions = options.filter(op => op.value !== initialValue);
     return (
-        <span onClick={toggleDropDown} className={`quarter-dropdown-${dropDownActive ? 'open' : 'closed'}`}>
-            <span >{defaultVal.label}</span>
-            <ul className="quarter-options" >
-                {options.map((option: Option<T>, i) => (<DropDownOption<T> key={i} option={option} optionAction={clickedOnOption} />))}
+        <span onClick={toggleDropDown} className={`dropdown dropdown-${dropDownActive ? 'open' : 'closed'}`}>
+            <span className="option-chosen" >{initialOption?.label} <i className="fas fa-chevron-down"></i>
+            </span>
+            <ul className="options" >
+                {leftOverOptions.map((option: Option<T>, i) => (<DropDownOption<T> key={i} option={option} optionAction={clickedOnOption} />))}
             </ul>
+
         </span>
     )
 }
--- client/src/components/QuarterSelect/QuarterSelect.tsx
+++ client/src/components/QuarterSelect/QuarterSelect.tsx
@@ -1,22 +1,60 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { useEffect, FC } from 'react'
+import { connect, ConnectedProps, Options } from 'react-redux'
 import { AppState } from '../../redux'
 import DropDownMenu, { Option } from "../DropDownMenu/DropDownMenu"
-export const QuarterSelect = () => {
+import { fetchQuarters, setQuarter } from "../../redux/actions";
+import { QuarterSeasons } from '../../../../shared/types';
+import "./QuarterSelect.scss"
+
+const mapStateToProps = (state: AppState) => ({
+    availableQuarters: state.quarterState.availableQuarters,
+    currentQuarter: state.quarterState.selectedQuarter
+})
+
+const mapDispatchToProps = {
+    fetchQuarters,
+    setQuarter
+}
+
+const connected = connect(mapStateToProps, mapDispatchToProps);
+
+type reduxProps = ConnectedProps<typeof connected>
+
+
+
+const QuarterSelect: FC<reduxProps> = ({ fetchQuarters, availableQuarters, currentQuarter, setQuarter }) => {
+
+    useEffect(() => {
+        fetchQuarters();
+    }, [])
+
+    let quarters: Option<QuarterSeasons>[] = [];
+    for (const season in availableQuarters) {
+        const element = availableQuarters[season as QuarterSeasons];
+        quarters.push({ value: season as QuarterSeasons, label: `${season.charAt(0).toUpperCase() + season.slice(1)} ${element?.year}` })
+        quarters.push({ value: season as QuarterSeasons, label: `${season.charAt(0).toUpperCase() + season.slice(1)} ${element?.year}` })
+        quarters.push({ value: season as QuarterSeasons, label: `${season.charAt(0).toUpperCase() + season.slice(1)} ${element?.year}` })
+        quarters.push({ value: season as QuarterSeasons, label: `${season.charAt(0).toUpperCase() + season.slice(1)} ${element?.year}` })
+        quarters.push({ value: season as QuarterSeasons, label: `${season.charAt(0).toUpperCase() + season.slice(1)} ${element?.year}` })
+        quarters.push({ value: season as QuarterSeasons, label: `${season.charAt(0).toUpperCase() + season.slice(1)} ${element?.year}` })
+
+    }
+
+    let onClick = (season: QuarterSeasons) => { setQuarter(season) }
     return (
         <div>
-            <label htmlFor=""></label>
-
+            <div><label htmlFor="">For Quarter</label></div>
+            <span>
+                <DropDownMenu<QuarterSeasons>
+                    initialValue={currentQuarter ? currentQuarter : "fall"}
+                    options={quarters}
+                    onClickOption={onClick}
+                />
+                <button >Key Dates</button>
+            </span>
         </div>
     )
 }
 
-const mapStateToProps = (state: AppState) => ({
 
-})
-
-const mapDispatchToProps = {
-
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(QuarterSelect)
+export default connected(QuarterSelect)
