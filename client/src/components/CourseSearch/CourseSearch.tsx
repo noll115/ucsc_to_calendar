bb2f5d77fba4f11be4aa5e0159e21c11597d45a2
Noel Gomez <nollgmz4@gmail.com>
2020-07-17 20:23:41 -0700

    added course search

--- client/src/components/CourseSearch/CourseSearch.tsx
+++ client/src/components/CourseSearch/CourseSearch.tsx
@@ -1,13 +1,17 @@
-import React, { Component, FC } from 'react'
+import React, { FC } from 'react'
 import { connect, ConnectedProps } from 'react-redux'
 import { AppState } from 'src/redux'
+import "./CourseSearch.scss"
 
-const mapStateToProps = (state: AppState) => ({
-
-})
+const mapStateToProps = (state: AppState) => {
+    let { selectedQuarter } = state.quarterState;
+    return {
+        courses: state.courseState[selectedQuarter]
+    }
+}
 
 const mapDispatchToProps = {
-
+    
 }
 
 const connected = connect(mapStateToProps, mapDispatchToProps);
@@ -16,10 +20,15 @@ type reduxProps = ConnectedProps<typeof connected>
 
 
 
-const CourseSearch: FC<reduxProps> = () => {
+const CourseSearch: FC<reduxProps> = ({courses}) => {
     return (
-        <div>
-
+        <div className="courseSearch">
+            <label htmlFor="courseInput" className="label">Add Class</label>
+            <p className="hint">e.g cse, cse 3, cse 3 - 01</p>
+            <div className="courseInput">
+                <i className="fas fa-search"></i>
+                <input type="text" name="course input" id="courseInput" />
+            </div>
         </div>
     )
 }
